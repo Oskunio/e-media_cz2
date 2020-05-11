e91a928d4575500af21873429e9b135c8f12af37
jimmeryn <jimmeryn@gmail.com>
2020-05-11 14:18:10 +0200

    de/encryption returning de/encrypted message

--- rsa_algorithm.py
+++ rsa_algorithm.py
@@ -1,6 +1,20 @@
-def encrypt(key, filename):
+
+
+# Encrypt
+# message - the 'message' to be encoded
+# n - public key(part 1)
+# e - public key(part 2)
+# returns encrypted message
+def encrypt(m, n, e):
     print("Encription\n")
+    return pow(m, e, n)
 
 
-def decrypt(key, filename):
+# Decrypt
+# message - the 'message' to be decoded
+# n - private key
+# e - public key(part 1)
+# returns decrypted message
+def decrypt(c, d, n):
     print("Decription\n")
+    return pow(c, d, n)
