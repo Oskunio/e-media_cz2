b846a3e3736ced876d8c57aa5747b21519b4f7e8
jimmeryn <jimmeryn@gmail.com>
2020-05-11 14:15:50 +0200

    generate keys when files dont exist

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from keygen import makeKeyFiles
 import rsa_algorithm
 import test
+import os
 
 if __name__ == '__main__':
     print("RSA")
-    print(makeKeyFiles("name", 1024))
+    if not os.path.exists('publickey.txt') and not os.path.exists('privatekey.txt'):
+        makeKeyFiles(1024)
