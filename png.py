8d9b48544d0511e9626fe72cc62daa5e4986fe5f
Oskar <podgrzyb123@gmail.com>
2020-05-16 16:15:59 +0200

    deszyfracja rsa do poprawy?

--- png.py
+++ png.py
@@ -10,7 +10,7 @@ def HexStringToPNG(filename, newFile):
         file.write(data)
     file.close()
 
-def encryptPNG(filename1,filename2, n, e, keySize):
+def encryptPNG(filename1,filename2, n, e, blockSize):
     handler = open(filename1, 'rb')
     hexFile = handler.read().hex()
     posInText = hexFile.find("49444154")
@@ -23,30 +23,62 @@ def encryptPNG(filename1,filename2, n, e, keySize):
         newIDAT = ''
         i = 0
         while i < realLength:
-            block = idatHex[i:i+4]
-            i = i+4
-            encryptedBlock = encryptBlock(block,n,e)
+            block = idatHex[i:i+blockSize]
+            i = i+blockSize
+            encryptedBlock = encryptBlock(block,n,e,blockSize)
             newIDAT += encryptedBlock
         newIdatLength = int(len(newIDAT) / 2)
         print(newIdatLength)
         newIdatLengthHex = format(newIdatLength,'x')
-        while len(newIdatLengthHex) != 8:
+        while len(newIdatLengthHex) % 8 != 0:
             newIdatLengthHex = '0' + newIdatLengthHex
         newFile = hexFile[0:(posInText-8)] + newIdatLengthHex + hexFile[posInText:(posInText+8)]  + newIDAT + hexFile[(posInText+realLength):]
         HexStringToPNG(filename2, newFile)
 
-def encryptBlock(block,n,e):
-    #lefBracket = '5b'
-    #rightBracket = '5d'
+def encryptBlock(block,n,e,blockSize):
+
     blockInt= int(block,16)
     encryptedBlock = rsa_algorithm.encrypt(blockInt,n,e)
     hexBlock = format(encryptedBlock,'x')
-    length = len(hexBlock)
-    if length % 2 != 0:
+    leng = len(hexBlock) # 256
+    while len(hexBlock) % blockSize != 0:
         hexBlock = '0' + hexBlock
     return hexBlock
 
+def decryptPNG(filename1,filename2, n, e):
+    handler = open(filename1, 'rb')
+    hexFile = handler.read().hex()
+    posInText = hexFile.find("49444154")
 
+    if posInText != -1:
+        length = hexFile[(posInText - 8):posInText]
+        chunkLengthDec = int(length, 16)
+        realLength = 2 * chunkLengthDec
+        idatHex = hexFile[(posInText + 8):(posInText + 8 + realLength)]
+        newIDAT = ''
+        i = 0
+        while i < realLength:
+            block = idatHex[i:i + 256]
+            i = i + 256
+            encryptedBlock = decryptBlock(block, n, e)
+            newIDAT += encryptedBlock
+        newIdatLength = int(len(newIDAT) / 2)
+        print(newIdatLength)
+        newIdatLengthHex = format(newIdatLength, 'x')
+        while len(newIdatLengthHex) % 8 != 0:
+            newIdatLengthHex = '0' + newIdatLengthHex
+        newFile = hexFile[0:(posInText - 8)] + newIdatLengthHex + hexFile[posInText:(posInText + 8)] + newIDAT + hexFile[(posInText + realLength):]
+        HexStringToPNG(filename2, newFile)
+
+def decryptBlock(block,n,e):
+
+    blockInt= int(block,16)
+    encryptedBlock = rsa_algorithm.decrypt(blockInt,n,e)
+    hexBlock = format(encryptedBlock,'x')
+    length = len(hexBlock) #powinno być tyle ile blockSize w encryptPNG czyli 64
+    if length % 2 != 0:
+        hexBlock = '0' + hexBlock
+    return hexBlock
 
 
 
--- test.py
+++ test.py
@@ -1,22 +1,34 @@
 import keygen
 import png
+import rsa_algorithm
 
 filename1 = './images/Square.png'
-filename2 = './images/newSquare.png'
-filename3 = './images/dog.png'
-filename4 = './images/newDog.png'
-filename5 = './images/tygrys.png'
-filename6 = './images/newTygrys.png'
+filename2 = './images/encryptedSquare.png'
+filename3 = './images/decryptedSquare.png'
+filename4 = './images/Dog.png'
+filename5 = './images/encryptedDog.png'
+filename6 = './images/decryptedDog.png'
 
-keyLength = 10
+keyLength = 512
 key = keygen.generateKey(keyLength)
 publicKey = key[0]
 privateKey = key[1]
 n = publicKey[0]
 e = publicKey[1]
 
-png.encryptPNG(filename1, filename2, n, e, keyLength)
-png.encryptPNG(filename3, filename4, n, e, keyLength)
-png.encryptPNG(filename5, filename6, n, e, keyLength)
+liczba = 10
+
+zakodowana = rsa_algorithm.encrypt(liczba,n,e)
+odkodowana = rsa_algorithm.decrypt(zakodowana,privateKey,e)
+
+print('odkodowana:')
+print(odkodowana)
+
+
+
+png.encryptPNG(filename1, filename2, n, e,  64)
+png.decryptPNG(filename2, filename3, privateKey, e)
+#png.encryptPNG(filename4, filename5, n, e,  64)
+#png.decryptPNG(filename5, filename6, privateKey, e)
 
 
